4e851c4fbd1c62a0289d40128d3137688cc2f5c6
LuisNavasArg <lpipnavas@gmail.com>
2018-02-28 12:38:02 -0300

    gregamos logo :q !

--- js/apiEmiliano.js
+++ js/apiEmiliano.js
@@ -1,8 +1,9 @@
 self.addEventListener("load", inicio);
 
 function inicio() {
-    var menues = ["INICIO", "QUIENES SOMOS", "¿QUÉ HACEMOS?", "PRENSA", "LINKS"];
-    var ids = ["inicio", "quienes", "hacemos", "prensa", "links"];
+   //Inicio, quienes somos, enlaces recomendados, documentos de interés, contactos
+    var menues = ["INICIO", "QUIENES SOMOS", "ENLACES", "DOCUMENTOS", "CONTACTOS"];
+    var ids = ["inicio", "quienes", "enlaces", "documentos", "contactos"];
     for (var a = 0; a < menues.length; a++) {
         var btn = document.createElement("div");
         var t = document.createTextNode(menues[a]);
@@ -12,15 +13,41 @@ function inicio() {
         btn.addEventListener("mouseover", mostrarMas);
         document.body.appendChild(btn);
     }
+    var til = "Somos una organización que reúne más de 20 años en la defensa y promoción de los derechos de las personas con discapacidad y otros grupos considerados cocialmente vulnerables.Creemos y trabajamos denodadamente en el marco de los derechos humanos con la convicción que la educación es la base fundamental para ls construcción de sociedades mas justas";
+
+    var pagina = document.createElement("div");
+    var text = document.createTextNode(til);
+    pagina.appendChild(text);
+    pagina.className = "paginas";
+    pagina.id = "textoSomos";
+    document.body.appendChild(pagina);
+
+
+
 
 }
 
 function mostrarMas() {
-    alert(this.id);
+    //alert(this.id);
     var menues = ["item1", "item2", "item3", "item4", "item5"];
     var ids = ["item1", "item2", "item3", "item4", "item5"];
 
 
+//creo la variable y le asigno el elemento
+//capturo por medio del id el elemento que quiero
+
+//var nuevo = document.createElement("div");
+//var yo = document.getElementById(this.id);
+
+//var t = document.createTextNode("");
+//nuevo.appendChild(t);
+
+//nuevo.className = "menu";
+
+//document.body.appendChild(nuevo);
+//document.body.appendChild(yo);
+
+/*
     for (var a = 0; a < menues.length; a++) {
         //creo la variable y le asigno el elemento
         //capturo por medio del id el elemento que quiero
@@ -35,5 +62,6 @@ function mostrarMas() {
 
         document.body.appendChild(nuevo);
         document.body.appendChild(yo);
-    }
+    }*/
+
 }
